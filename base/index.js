18756a0117e235a7834c9bf84f33653ed79f83df
Alex Marshall <services@marshallprogramming.com>
2020-09-02 11:32:13 +0100

    finished course

--- base/index.js
+++ base/index.js
@@ -1,16 +1,26 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import bodyParser from 'body-parser';
-import routes from './src/routes/crmRoutes';
+import express from "express";
+import mongoose from "mongoose";
+import bodyParser from "body-parser";
+import RateLimit from "express-rate-limit";
+import helmet from "helmet";
+import routes from "./src/routes/crmRoutes";
 
 const app = express();
 const PORT = 3000;
 
+// helmet setup
+app.use(helmet());
+
+// Rate limit setup
+const limiter = new RateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // limit of number of request per IP
+  delayMS: 0, // disables delays
+});
+
 // mongoose connection
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/CRMdb', {
-    useMongoClient: true
-});
+mongoose.connect("mongodb://localhost/CRMdb");
 
 // bodyparser setup
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -19,12 +29,10 @@ app.use(bodyParser.json());
 routes(app);
 
 // serving static files
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-app.get('/', (req, res) =>
-    res.send(`Node and express server is running on port ${PORT}`)
+app.get("/", (req, res) =>
+  res.send(`Node and express server is running on port ${PORT}`)
 );
 
-app.listen(PORT, () =>
-    console.log(`your server is running on port ${PORT}`)
-);
+app.listen(PORT, () => console.log(`your server is running on port ${PORT}`));
